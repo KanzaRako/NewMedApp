5dce7679bf91d1b253e5bfe71214cc2d714c2d64
KanzaRako <kanzarakotobe@gmail.com>
2020-11-21 22:29:42 +0100

    Barre de recherche medecins et structuration du code

---
+++ src/main/resources/static/script/menu.js
@@ -0,0 +1,9 @@
+
+    $(".menu-toggle-btn").click(function(){
+    $(this).toggleClass("fa-times");
+    $(".navigation-menu").toggleClass("active");
+});
+
+
+
+
--- src/main/resources/static/script/scripts.js
+++ src/main/resources/static/script/scripts.js
@@ -1,10 +1,10 @@
-var search_input = document.querySelector("#search_input");
+var search_input = document.querySelector("#searchInput");
 
 search_input.addEventListener("keyup", function(e){
     var span_items = document.querySelectorAll(".table_body .specialite span");
     var table_body = document.querySelector(".table_body ul");
     var search_item = e.target.value.toLowerCase();
-
+    console.log("search item : " + search_item);
     span_items.forEach(function(item){
         if(item.textContent.toLowerCase().indexOf(search_item) != -1){
             item.closest("li").style.display = "block";
@@ -15,3 +15,4 @@ search_input.addEventListener("keyup", function(e){
     })
 
 });
+
